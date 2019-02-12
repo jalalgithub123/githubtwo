071c3bb538dd066742e2f57b6ddb33163af6d51d
jalal8068 <jalalahamad9@gmail.com>
2019-02-12 20:44:06 +0530

    git hub second project commit

---
+++ src/com/github/two/BaseClass.java
@@ -0,0 +1,10 @@
+package com.github.two;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is base class code");
+	}
+
+}
